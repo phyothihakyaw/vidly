fa340f20034e1d06c7d247e02c0782a04650e083
Phyo Thiha Kyaw <mr.phyothihakyaw9@outlook.com>
2020-09-19 03:23:49 +0630

    Add movies index page

---
+++ Vidly/Vidly.Web/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Vidly.Web.Data;
+
+namespace Vidly.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET : Movies
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.Include(m => m.Genre).ToListAsync();
+
+            return View(movies);
+        }
+    }
+}
