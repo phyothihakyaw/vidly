ab31431ee840775eb2ff1ee047033a6213fd723c
Phyo Thiha Kyaw <mr.phyothihakyaw9@outlook.com>
2020-09-26 17:17:41 +0630

    Remove NLog

--- Vidly/Vidly.Web/Program.cs
+++ Vidly/Vidly.Web/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using NLog.Web;
-using System;
 
 namespace Vidly.Web
 {
@@ -9,26 +7,7 @@ namespace Vidly.Web
     {
         public static void Main(string[] args)
         {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var isDev = string.Equals(environment, "development", StringComparison.InvariantCultureIgnoreCase);
-            var configFileName = isDev ? "nlog.Development.config" : "nlog.config";
-
-            var logger = NLogBuilder.ConfigureNLog(configFileName).GetCurrentClassLogger();
-
-            logger.Info("Starting application. {Environment}, {LoggingConfigurationFileName}", environment, configFileName);
-
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception e)
-            {
-                logger.Error(e, "Failed to start application", e);
-            }
-            finally
-            {
-                NLog.LogManager.Shutdown();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
